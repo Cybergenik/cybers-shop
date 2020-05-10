fc8bfe842c316105d18a9a59f7ae23fbfe7d3b62
Luciano Remes <cybergenik@gmail.com>
2020-05-09 21:56:33 -0600

    making it more readable, removing as many echos as possible

--- index.php
+++ index.php
@@ -38,7 +38,6 @@ session_start();
     </div>
     <br>
     <?php
-        //display error messages and missed fields
         if(!empty($_POST)){
             //Make sure fields are filled
             if(empty($_POST['user']) || empty($_POST['pass'])){
@@ -63,61 +62,59 @@ session_start();
             if($_GET['acc'] == 1)
                 echo '<div class="flex-container"><h3>Account Created, please login</h3></div>';
         }
-        if (!isset($_SESSION['auth'])){
-            echo '
-            <h2 style="border-bottom: 2px solid; width: 15%; color: #ff7a7a; margin-bottom: 2%">Log in:</h2>
-            <form class="flex-container" action="index.php" method="post">
-            <label for="user">Username:</label>        
-                <input class="myin" class="input" name="user" id="user" type="text" placeholder="Username">
-            <label for="pass">Password:</label>
-                <input class="myin" class="input" name="pass" id="pass" type="password" placeholder="Password">
-                <div class="flex-container2">
+    ?>
+    <?php if(!isset($_SESSION['auth'])):?>
+        <h2 style="border-bottom: 2px solid; width: 15%; color: #ff7a7a; margin-bottom: 2%">Log in:</h2>
+        <form class="flex-container" action="index.php" method="post">
+        <label for="user">Username:</label>        
+            <input class="myin" class="input" name="user" id="user" type="text" placeholder="Username">
+        <label for="pass">Password:</label>
+            <input class="myin" class="input" name="pass" id="pass" type="password" placeholder="Password">
+            <div class="flex-container2">
                 <input class="mybutton" type="reset">
                 <input class="mybutton"  type="submit" value="Login">
-                </div>
-            </form>
-            <div class="flex-container" style="margin-top: 2%">
-            <button class="mybutton" onClick="document.location.href=\'create-account.php\'">Create Account</button>
             </div>
-            ';
-        }
-        //Display Access Granted
-        elseif (isset($_SESSION['auth'])){
-            echo "<h2 style='color: #ff7a7a;'>Welcome to <u>Cyber's</u> Gamer Shop</h2>
-            <br><br><h2>Gamers Choices:</h2><br>
-            <div class='flex-container2'>
-            ";
-            include_once('includes/db.php');  
-            $conn = new Conn();
-            $select = "SELECT * FROM web_poll";
-            $results = mysqli_query($conn->getConn(), $select);
-            $data = Array(mysqli_fetch_array($results));
+        </form>
+        <div class="flex-container" style="margin-top: 2%">
+            <button class="mybutton" onClick="document.location.href=\'create-account.php\'">Create Account</button>
+        </div>
+    <?php elseif(isset($_SESSION['auth'])):?>
+        <h2 style='color: #ff7a7a;'>Welcome to <u>Cyber's</u> Gamer Shop</h2>
+        <br><br><h2>Gamers Choices:</h2><br>
+        <div class='flex-container2'>
+        <?php
+        //Loading data for Graphs into JS
+        include_once('includes/db.php');  
+        $conn = new Conn();
+        $select = "SELECT * FROM web_poll";
+        $results = mysqli_query($conn->getConn(), $select);
+        $data = Array(mysqli_fetch_array($results));
 
-            $league = ($data[0]['league'] / $data[0]['moba_total']) * 100;
-            $dota = ($data[0]['dota'] / $data[0]['moba_total']) * 100;
-            $smite = ($data[0]['smite'] / $data[0]['moba_total']) * 100;
-            $hots = ($data[0]['hots'] / $data[0]['moba_total']) * 100;
+        $league = ($data[0]['league'] / $data[0]['moba_total']) * 100;
+        $dota = ($data[0]['dota'] / $data[0]['moba_total']) * 100;
+        $smite = ($data[0]['smite'] / $data[0]['moba_total']) * 100;
+        $hots = ($data[0]['hots'] / $data[0]['moba_total']) * 100;
 
-            $mobasData = array( 
-                array("label"=>"League of Legends", "y"=>$league),
-                array("label"=>"DOTA", "y"=>$dota),
-                array("label"=>"Smite", "y"=>$smite),
-                array("label"=>"Heroes of the Storm", "y"=>$hots),
-            );
-            $csgo = ($data[0]['csgo'] / $data[0]['shooter_total']) * 100;
-            $overwatch = ($data[0]['overwatch'] / $data[0]['shooter_total']) * 100;
-            $cod = ($data[0]['cod'] / $data[0]['shooter_total']) * 100;
-            $fortnite = ($data[0]['fortnite'] / $data[0]['shooter_total']) * 100;
+        $mobasData = array( 
+            array("label"=>"League of Legends", "y"=>$league),
+            array("label"=>"DOTA", "y"=>$dota),
+            array("label"=>"Smite", "y"=>$smite),
+            array("label"=>"Heroes of the Storm", "y"=>$hots),
+        );
 
-            $shootersData = array( 
-                array("label"=>"Counter Strike", "y"=>$csgo),
-                array("label"=>"Overwatch", "y"=>$overwatch),
-                array("label"=>"Call Of Duty", "y"=>$cod),
-                array("label"=>"Fortnite", "y"=>$fortnite),
-            );
-            
-            echo "<br><div id='mobasContainer' style='height: 370px; width: 50%; text-color: white;'></div>";
-            echo '
+        $csgo = ($data[0]['csgo'] / $data[0]['shooter_total']) * 100;
+        $overwatch = ($data[0]['overwatch'] / $data[0]['shooter_total']) * 100;
+        $cod = ($data[0]['cod'] / $data[0]['shooter_total']) * 100;
+        $fortnite = ($data[0]['fortnite'] / $data[0]['shooter_total']) * 100;
+
+        $shootersData = array( 
+            array("label"=>"Counter Strike", "y"=>$csgo),
+            array("label"=>"Overwatch", "y"=>$overwatch),
+            array("label"=>"Call Of Duty", "y"=>$cod),
+            array("label"=>"Fortnite", "y"=>$fortnite),
+        );?>
+        
+        <br><div id='mobasContainer' style='height: 370px; width: 50%; text-color: white;'></div>
             <script>
                 var mobas = new CanvasJS.Chart("mobasContainer", {
                 animationEnabled: true,
@@ -135,46 +132,40 @@ session_start();
                     indexLabelFontColor: "white",
                     yValueFormatString: "#,##0.00\"%\"",
                     indexLabel: "{label} ({y})",
-                    dataPoints: '.json_encode($mobasData, JSON_NUMERIC_CHECK).'
+                    dataPoints: <?php echo json_encode($mobasData, JSON_NUMERIC_CHECK) ?>
                 }]
             });
             mobas.render();
             </script>
-            ';
-            echo "<br><br><div id='shootersContainer' style='height: 370px; width: 50%; color: white;'></div>";
-            echo '
+            <br><br><div id='shootersContainer' style='height: 370px; width: 50%; color: white;'></div>
             <script>
                 var shooters = new CanvasJS.Chart("shootersContainer", {
-                animationEnabled: true,
-                backgroundColor: null,
-                colorSet:  "colorSet1",
-                title: {
-                    text: "Shooters Popularity",
-                    fontColor: "white"
-                },
-                subtitles: [{
-                    text: "2020",
-                    fontColor: "white"
-                }],
-                data: [{
-                    type: "pie",
-                    indexLabelFontColor: "white",
-                    yValueFormatString: "#,##0.00\"%\"",
-                    indexLabel: "{label} ({y})",
-                    dataPoints: '.json_encode($shootersData, JSON_NUMERIC_CHECK).'
-                }]
-            });
-            shooters.render();
+                    animationEnabled: true,
+                    backgroundColor: null,
+                    colorSet:  "colorSet1",
+                    title: {
+                        text: "Shooters Popularity",
+                        fontColor: "white"
+                    },
+                    subtitles: [{
+                        text: "2020",
+                        fontColor: "white"
+                    }],
+                    data: [{
+                        type: "pie",
+                        indexLabelFontColor: "white",
+                        yValueFormatString: "#,##0.00\"%\"",
+                        indexLabel: "{label} ({y})",
+                        dataPoints: <?php echo json_encode($shootersData, JSON_NUMERIC_CHECK)?>
+                        }]
+                });
+                shooters.render();
             </script>
-            </div>
-            ';
-            $conn->closeConn();
-            echo '
-                <div class="flex-container">
-                <a class="mybutton" style="color: #151D21;" href="https://cybers-shop.herokuapp.com/poll" target="_blank">Take the Survey!</a> 
-                </div>
-            ';
-        }
-    ?>
+        </div>
+        <?php $conn->closeConn();?>
+        <div class="flex-container">
+        <a class="mybutton" style="color: #151D21;" href="https://cybers-shop.herokuapp.com/poll" target="_blank">Take the Survey!</a> 
+        </div>
+    <?php endif?>
 </body>
 </html>
